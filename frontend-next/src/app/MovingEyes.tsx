c41baa29d9f9ec693e6779c932308fcb84d9dac0
Maximilian Mallweger <maxi@MacBook-Air-von-Maximilian.local>
2023-05-24 13:35:37 +0200

    exp

--- frontend-next/src/app/MovingEyes.tsx
+++ frontend-next/src/app/MovingEyes.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 export default function MovingEyes() {
+  const angle = 60;
   return (
     <>
       <div className="flex justify-start items-center p-5 space-x-5">
@@ -7,7 +10,7 @@ export default function MovingEyes() {
         </div>
 
         <div className="flex h-24 w-24 bg-slate-400 rounded-full justify-center">
-          <div className="bg-blue-400 flex pt-2 rotate-[90deg]">
+          <div className="bg-blue-400 flex pt-2">
             <div className="h-12 w-12 bg-slate-600 rounded-full"></div>
           </div>
         </div>

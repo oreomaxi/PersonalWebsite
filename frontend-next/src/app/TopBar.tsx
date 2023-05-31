c8aa824dc91e57ffbf6f377bda0794c7840c9267
Maximilian Mallweger <mallweger.m@gmail.com>
2023-05-31 21:57:57 +0200

    navbar functionality

--- frontend-next/src/app/TopBar.tsx
+++ frontend-next/src/app/TopBar.tsx
@@ -6,18 +6,32 @@ import { usePathname } from "next/navigation";
 
 export default function TopBar() {
   const pathName = usePathname();
-
+  function getLinkItems() {}
   return (
     <>
       <nav className="w-full flex justify-between p-4 items-center shadow-md">
         <Link href={"/"}>
           <div className="bg-slate-400 p-2">logo</div>
         </Link>
-        <div className="hidden sm:flex justify-between w-1/4 items-center">
-          <Link href={"/"} className="bg-slate-400 p-2 rounded-lg">
+        <div className="hidden sm:flex gap-5 justify-between items-center">
+          <Link
+            href={"/"}
+            className={
+              "bg-slate-400 p-2 rounded-lg" +
+              (pathName === "/" ? " text-red-500 " : " text-gray-500 ")
+            }
+          >
             Home
           </Link>
-          <Link href={"/"}>Blog</Link>
+          <Link
+            href={"/blog"}
+            className={
+              "bg-slate-400 p-2 rounded-lg" +
+              (pathName === "/blog" ? " text-red-500 " : " text-gray-500 ")
+            }
+          >
+            Blog
+          </Link>
           <Link href={"/"}>Home</Link>
           <Link href={"/"}>pathname {pathName}</Link>
         </div>

288ee26e430138564d891cdfd1b4d839cf8e01e1
Maximilian Mallweger <mallweger.m@gmail.com>
2023-06-02 20:00:15 +0200

    some tinkering with design

--- frontend-next/src/app/page.tsx
+++ frontend-next/src/app/page.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <div className="grid grid-cols-2 p-5 h-screen gap-5">
+        <div className="flex justify-center items-center w-full bg-slate-50 h-3/4 rounded-xl shadow-md">
+          <div>Col 1</div>
+          <div>below</div>
+        </div>
+        <div className="w-full bg-slate-50 h-3/4 rounded-xl shadow-md">
+          Col 2
+        </div>
+      </div>
     </>
   );
 }

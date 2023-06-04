cb9af03ee8bf18589e726d20d7bd8e7bd14d6148
Maximilian Mallweger <mallweger.m@gmail.com>
2023-06-04 18:46:39 +0200

    some grid testing

--- frontend-next/src/app/page.tsx
+++ frontend-next/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="grid grid-cols-2 p-5 h-screen gap-5">
+      <div className="grid grid-cols-1 p-5 h-screen lg:grid-cols-2 gap-1">
         <div className="flex justify-center items-center w-full bg-slate-50 h-3/4 rounded-xl shadow-md">
           <div>Col 1</div>
           <div>below</div>
@@ -11,6 +11,11 @@ export default function Home() {
         <div className="w-full bg-slate-50 h-3/4 rounded-xl shadow-md">
           Col 2
         </div>
+      </div>
+
+      <div className="grid grid-cols-1 p-5 lg:grid-cols-2 gap-1">
+        <div className="bg-slate-50">Div 1</div>
+        <div className="bg-slate-50">Div 2</div>
       </div>
     </>
   );
